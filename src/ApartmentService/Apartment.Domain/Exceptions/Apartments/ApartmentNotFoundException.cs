afb85f45874f12d53a4a49323cf334642589af39
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-11-26 20:37:22 +0500

    Completed Domain Layer

---
+++ src/ApartmentService/Apartment.Domain/Exceptions/Apartments/ApartmentNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace Apartment.Domain.Exceptions.Apartments;
+
+public class ApartmentNotFoundException : NotFoundException
+{
+    public ApartmentNotFoundException()
+    {
+        TitleMessage = "Apartment not found!";
+    }
+}
