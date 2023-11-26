54b79368df8ed51895734ff2735a6cff02eac7b3
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-11-27 01:36:05 +0500

    added exception

---
+++ src/ApartmentService/Apartment.Domain/Exceptions/Apartments/ApartmentAlreadyExistsException.cs
@@ -0,0 +1,9 @@
+﻿namespace Apartment.Domain.Exceptions.Apartments;
+
+public class ApartmentAlreadyExistsException : NotFoundException
+{
+    public ApartmentAlreadyExistsException()
+    {
+        TitleMessage = "Apartment already exists!";
+    }
+}
