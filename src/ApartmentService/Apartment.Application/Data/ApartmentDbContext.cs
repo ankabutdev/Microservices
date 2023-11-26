eac7485dad330551973b809da5796858d8861cd7
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-11-26 20:51:27 +0500

    added dbContext in ApartmentService

---
+++ src/ApartmentService/Apartment.Application/Data/ApartmentDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Apartment.Application.Data;
+
+public class ApartmentDbContext : DbContext
+{
+    public ApartmentDbContext(DbContextOptions<ApartmentDbContext> options)
+        : base(options) { }
+
+    //public DbSet<Apartment> Apartments { get; set; }
+}
