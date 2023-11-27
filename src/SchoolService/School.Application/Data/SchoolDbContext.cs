06a11c8e1dd21c53a54afe234765e9d26a5f3451
Salohiddin Sunnatov <Ankabut571@gmail.com>
2023-11-27 06:20:06 +0500

    Completed Data and Project References

---
+++ src/SchoolService/School.Application/Data/SchoolDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using School.Domain.Entities.Students;
+
+namespace School.Application.Data;
+
+public class SchoolDbContext : DbContext
+{
+    public SchoolDbContext(DbContextOptions<SchoolDbContext> options)
+        : base(options) { }
+
+    public DbSet<SchoolModel> Schools { get; set; }
+}
